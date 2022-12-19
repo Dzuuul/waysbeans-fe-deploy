2668d9eea8ad6b3a9710bcb8e587f0bd31834fd0
Moh Dzulfikri Maulana <dzuuuuul2@gmail.com>
2022-12-19 18:54:04 +0700

    first

---
+++ src/config/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+  // baseURL: "http://localhost:5000/waysbeans/",
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.common["Authorization"];
+  }
+};
